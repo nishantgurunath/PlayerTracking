e58d369d98746e2c0b246f45e17bcb5c969d390d
Aniruddh1997 <aniruddhshetty@gmail.com>
2019-10-27 19:02:56 -0400

    divided detection module

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PlayerTracking
+Use the below link to dowload the dataset and put the data in dataset/NCAA/ folder (create new folder)
+http://www.cs.toronto.edu/~namdar/sports_course/ncaa/data.7z
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import cv2
-from detection.detector import detect
+from detection.aniruddh.detector import detect
 
 data_dir = './dataset/NCAA/'
 game_name = '0En5pOUZN5M'
@@ -11,7 +11,16 @@ img_num = 1
 for i in range(5):
 	im_path = data_dir + game_name + '/' + clip_name + '/' + '{:02}.png'.format(i + 1)
 	im = cv2.imread(im_path)
-	detect(im)
+	rects = detect(im)
+
+	for i, (x, y, w, h) in enumerate(rects):
+		# if weights[i] < 0.7:
+		#     continue
+		cv2.rectangle(im, (x,y), (x+w,y+h),(0,255,0),2)
+
+	cv2.imshow("preview", im)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 
 # Ground truth boxes
 # bb_path = data_dir + game_name + '/' + clip_name + '/' + '{:02}_info.csv'.format(img_num)
