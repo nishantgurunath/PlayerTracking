3270cf3d954a7b7284eacb2744edb15b29a34ebf
Kyungzun Rim <krim@andrew.cmu.edu>
2019-11-07 16:36:21 -0500

    video version

--- detection/Zun/detector.py
+++ detection/Zun/detector.py
@@ -22,6 +22,7 @@ class Detection:
         self.court_upper_color = np.array([hue+ 30,255,255])
         self.lower_blue = np.array([70,50,50])
         self.upper_blue = np.array([110,255,255])
+        self.jersey_color = np.array([])
    
     def load_image_folders(self):
         self.image_folders = [os.path.join(self.data_directory,name) for name in os.listdir(self.data_directory)] 
@@ -29,46 +30,50 @@ class Detection:
         
     def detect_people(self):
       # Save the image for one
-      image_files = [f for f in glob.glob(self.image_folders[0] + "/**/*.png",
-        recursive=True)]
-      csv_files =  [f for f in glob.glob(self.image_folders[0] + "/**/*.csv",
-        recursive=True)]
-      image_files.sort()
-      csv_files.sort()
-
-      # converting into hsv image
-      image = cv2.imread(image_files[0])
-      csv = csv_files[0]
+      # Reading the video 
+      vidcap  = cv2.VideoCapture('cutvideo.mp4')
+      success,image = vidcap.read()
+      count = 0
+      success = True
+      idx = 0
+      
       hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-      
-      # Mask with additional color
-      mask2 = cv2.inRange(hsv,self.lower_blue,self.upper_blue)
-      res2 = cv2.bitwise_and(image,image, mask=mask2)
-      # Mask with court color
-      mask1 = cv2.inRange(hsv,self.court_lower_color,self.court_upper_color)
-      res1 = cv2.bitwise_and(image,image, mask=mask1)
 
-      res = res1 + res2
-      # Convert hsv to gray
-      res_bgr = cv2.cvtColor(res1,cv2.COLOR_HSV2BGR)
-      res_gray = cv2.cvtColor(res1,cv2.COLOR_BGR2GRAY)
-      
-      # Defining a kernel to do morp
-      kernel = np.zeros((5,5),np.uint8)
-      thresh = cv2.threshold(res_gray,85,205,cv2.THRESH_BINARY_INV|cv2.THRESH_OTSU)[1]
-      thresh1 = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-      thresh2 = cv2.cornerHarris(res_gray,3,3,0.1)
-      contours, hierarchy = cv2.findContours(res_gray, cv2.RETR_TREE,
-          cv2.CHAIN_APPROX_SIMPLE)
+      #green range 
+      lower_green = np.array([40,40,40])
+      upper_green = np.array([70,255,255])
+
+      mask = cv2.inRange(hsv, lower_green, upper_green)
+      res = cv2.bitwise_and(image, image, mask = mask)
+
+      res_bgr = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+      res_gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+
+      kernel = np.ones((13,13), np.uint8)
+      thresh = cv2.threshold(res_gray,127,255,cv2.THRESH_BINARY_INV |
+          cv2.THRESH_OTSU)[1]
+      thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel) 
+
+      #find contours in threshold image
+      contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+      areas = [cv2.contourArea(c) for c in contours]
+      max_index = np.argmax(areas)
+      max_cnt=contours[max_index]
+      x,y,w,h = cv2.boundingRect(max_cnt)
+      cv2.rectangle(thresh,(x,y),(x+w,y+h),(100,255,0),2)
+
 
       plt.imshow(res_gray), plt.show()
-      plt.imshow(thresh), plt.show()
-      plt.imshow(thresh1), plt.show()
-      plt.imshow(thresh2), plt.show()
-      cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
-      plt.imshow(image), plt.show()
+
+
+      #plt.imshow(mac_cnt), plt.show()
+      #cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+      #plt.imshow(image), plt.show()
+    def get_jersey_color(self):
+      jersey_image = cv2.imread('france.jpeg')
 def main():
     obj = Detection()
+    obj.get_jersey_color()
     obj.load_image_folders()
     obj.detect_people()
 
